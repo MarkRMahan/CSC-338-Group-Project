23626d35f246e9459fa07cd137fb12c23f26e021
Mark R Mahan <36490225+MarkRMahan@users.noreply.github.com>
2018-04-27 16:19:22 -0500

    Add files via upload
    temporary

--- input_data/input_creator.py
+++ input_data/input_creator.py
@@ -2,46 +2,73 @@ import random
 '''Creates 100 random graphs to run dijkstra's on'''
 
 def main():
-    num_nodes = random.randint(10, 30)
+    num_nodes = 10 #random.randint(10, 1000)
     dictionary = {}
-    for i in range(num_nodes):
+    dictionary_to_read = {}
+    for i in range(num_nodes): #Node
         bound = random.randint(2, num_nodes)
-        for x in range(random.randint(1, bound) - 1, bound):
-            if x == i: # Prevents paths to the node itself
+        for x in range(random.randint(1, bound) - 1, bound): #Node i is connected to
+            #print(no_extra_paths)
+            if x == i: #Prevents paths to the node itself and multiple paths to the same node
                 continue
             else:
-                weight = random.randint(1,5)
+                weight = random.randint(1,8)
                 if i in dictionary:
-                    dictionary[i].append(" Node: {:<3} Cost: {:<3} |".format(x, weight))
+                    dictionary[i].append("{},{}".format(x, weight))
+                    dictionary_to_read[i].append(" Node: {:<3} Cost: {:<3} |".format(x, weight))
                 else:
-                    dictionary[i] = [" Node: {:<3} Cost: {:<3} |".format(x, weight)]
-
+                    dictionary[i] = ["{},{}".format(x, weight)]
+                    dictionary_to_read[i] = [" Node: {:<3} Cost: {:<3} |".format(x, weight)]
                 if x in dictionary:
-                    dictionary[x].append(" Node: {:<3} Cost: {:<3} |".format(i, weight))
+                    dictionary[x].append("{},{}".format(i, weight))
+                    dictionary_to_read[x].append(" Node: {:<3} Cost: {:<3} |".format(i, weight))
                 else:
-                    dictionary[x] = [" Node: {:<3} Cost: {:<3} |".format(i, weight)]
+                    dictionary[x] = ["{},{}".format(i, weight)]
+                    dictionary_to_read[x] = [" Node: {:<3} Cost: {:<3} |".format(i, weight)]
+
 
 
     no_duplicates = {}
     #Gets rid of duplicates
     for key,value in dictionary.items():
-        if key not in no_duplicates.keys():
-            no_duplicates[key] = value
-    
-    return no_duplicates
+        connections = set()
+        print(value)
+        for i in value:
+            value_prime = i.split(',')[0]
+            if value_prime not in connections:
+                no_duplicates[key] = value
+                connections.add(value_prime)
+
+    no_duplicates_to_read = {}
+    for key,value in dictionary_to_read.items():
+        if value not in no_duplicates_to_read.values():
+            no_duplicates_to_read[key] = value
+            
+
+    output = []
+    output.append(no_duplicates)
+    output.append(no_duplicates_to_read)
+    return output
 
 
 
 
 if __name__ == '__main__':
     for i in range(10):
+        dictionary = main()
         with open("input_{}.txt".format(i), "w") as f:
-            dictionary = main()
-
-            for key in sorted(dictionary.keys()):
+            for key in sorted(dictionary[0].keys()):
                 node = "{:^3}".format(key)
-                for connected in dictionary[key]:
+                for connected in dictionary[0][key]:
                     node += " " + connected
             
+                f.write(node + "\n")
+
+        with open("input_{}_read.txt".format(i), "w") as f:
+            for key in sorted(dictionary[1].keys()):
+                node = "{}".format(key)
+                for connected in dictionary[1][key]:
+                    node += " " + connected
+
                 f.write(node + "\n")
                 
