43afb23e158dca919eaeb351608ea3ce1412d15f
Taylor Cook <tmc97@live.missouristate.edu>
2018-05-09 11:56:37 -0500

    messing with dicts

--- Dijkstra'sOriginal/dijkstras.py
+++ Dijkstra'sOriginal/dijkstras.py
@@ -14,9 +14,9 @@ with open (sys.argv[1], 'r') as graph_doc:
     for line in graph_doc:
         values = [int(x) for x in line.replace(',', ' ').split()]
         graph_dict[values[0]] = [v for v in zip(values[1::2], values[2::2])]
-        
-#Now, here is Dijkstra's being dope
 
+#Now, here is Dijkstra's being dope
+print(graph_dict)
 visited_nodes = [1] #initializing visited array - c
 
 num_nodes = len(graph_dict) #making variable for length of dictionary - c
@@ -25,16 +25,16 @@ path_lengths = [-1, 0] + [1000000] * (num_nodes - 1) #initializing lengths array
 
 while len(visited_nodes) < num_nodes: # - n
     potential_edges = [] #empty list for potential edges to be checked    - c
- 
+
     #going through all of the visited nodes
-    for vertex in visited_nodes: #this whole loop will be around log n I believe 
+    for vertex in visited_nodes: #this whole loop will be around log n I believe
         for w, l in graph_dict[vertex]: #looking at data points for each vertex
             if w not in visited_nodes: #going through all of the not visited nodes
-                potential_edges.append((w, path_lengths[vertex] + l)) #add node to potential_edges to be checked     
+                potential_edges.append((w, path_lengths[vertex] + l)) #add node to potential_edges to be checked
     star = min(potential_edges, key=lambda n: n[1])#function to find the min
-            
+
     visited_nodes.append(star[0]) #add the edge to visited to start the loop over - c
-            
+
     path_lengths[star[0]] = star[1] #setting up to find the new value for the next iteration - c
-            
-print ",".join(str(l) for l in path_lengths[1:]) #print statement for the thing
+
+print (",".join(str(l) for l in path_lengths[1:])) #print statement for the thing
--- DijkstraParallel/dij_mp.py
+++ DijkstraParallel/dij_mp.py
@@ -7,10 +7,10 @@ import argparse
 import sys
 import time
 import multiprocessing as mp
+dict_list=[]
+dict = {}
 
-dict_list = []
-
-def dij(start, end, nodes, dict):
+def dij(start, end, nodes, dict, dict_list):
     for i in range(start, end):
         #Prepping for Dijkstra's Algorithm
         path_lengths = []
@@ -36,8 +36,9 @@ def dij(start, end, nodes, dict):
                 counter += 1
             else:
                 answer += str(counter)+"," +str(distance)
-        #dict[i] = answer
-        print(answer)
+        dict[i] = answer
+        dict_list[i] = dict[i]
+        #print(answer)
 
 def main():
     arg_parser = argparse.ArgumentParser(description='Print the given input file.')
@@ -52,7 +53,10 @@ def main():
         for element in index.split():
             if index.split()[0] != element:
                 nodes[index.split()[0]].append(element.split(","))
-
+    #dict_list = []
+    for node in range(len(nodes)+1):
+        dict_list.append(0)
+    #dict = {}
     num_processes = mp.cpu_count()
     each_process = (len(nodes)//num_processes) #Number of iterations each process completes
     processes = []
@@ -62,31 +66,34 @@ def main():
         if leftover != 0:
             if i == 1:
                 start = (i*2)-1
-                m = mp.Process(target = dij, args = (start, start +each_process+ 1, nodes))
+                m = mp.Process(target = dij, args = (start, start +each_process+ 1, nodes,
+                                                    dict, dict_list))
                 end = start+each_process+ 1
                 processes.append(m)
                 leftover -= 1
             else:
-                m = mp.Process(target = dij, args = (end, end + each_process+1, nodes))
+                m = mp.Process(target = dij, args = (end, end + each_process+1, nodes,
+                                                    dict, dict_list))
                 end = end + each_process + 1
                 processes.append(m)
                 leftover -= 1
         else:
             if i == 1:
-                m = mp.Process(target = dij, args = (end, end+each_process, nodes))
+                m = mp.Process(target = dij, args = (end, end+each_process, nodes,
+                                                    dict,dict_list))
                 end = end + each_process
                 processes.append(m)
             else:
-                m = mp.Process(target = dij, args = (end, end+each_process, nodes))
+                m = mp.Process(target = dij, args = (end, end+each_process, nodes,
+                                                    dict, dict_list))
                 end = end + each_process
                 processes.append(m)
-
-
     for process in processes:
         process.start()
     for process in processes:
         process.join()
 
+    print(dict)
     # for i in range(1, len(nodes)+1):    #Find each node to each other node paths
     #     dij(i, nodes)
 
